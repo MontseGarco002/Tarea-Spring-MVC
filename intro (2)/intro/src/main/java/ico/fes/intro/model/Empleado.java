b58de53b09b494bbd4828c1b9d9462c91a6d90ff
MontseGarco002 <128559638+MontseGarco002@users.noreply.github.com>
2023-05-22 23:02:49 -0600

    Add files via upload

---
+++ intro (2)/intro/src/main/java/ico/fes/intro/model/Empleado.java
@@ -0,0 +1,18 @@
+package ico.fes.intro.model;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+
+public class Empleado {
+    private int id;
+    private String nombre;
+    private double sueldo;
+    private String departamento;
+    private String fechaNacimiento ;
+
+}
